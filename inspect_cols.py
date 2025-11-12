08a934683a51172fdbd020e9b7d2d6a545ecd481
Chiragshetty2005 <chiraglab007@gmail.com>
2025-11-12 21:44:11 +0530

    deleted

--- inspect_cols.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-DF = pd.read_csv('heart_disease_uci_encoded_with_id.csv')
-print('columns:', DF.columns.tolist())
-row = DF[DF['id']==4].iloc[0]
-print('\nrow index:')
-for c in row.index:
-    print(c, '->', row[c])
